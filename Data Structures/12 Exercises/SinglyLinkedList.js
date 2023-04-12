9d62759be9298c9866021875bb63372cf3034de3
Jan Jelínek <janjelinek.jr@gmail.com>
2023-04-12 16:55:20 +0200

    added exercies SSL-push,pop,get,insert

---
+++ Data Structures/12 Exercises/SinglyLinkedList.js
@@ -0,0 +1,107 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    push(val) {
+        const newNode = new Node(val)
+        if (!this.head) {
+            this.head = newNode
+            this.tail = this.head
+        }
+        this.tail.next = newNode
+        this.tail = newNode
+        this.length++
+
+        return this
+    }
+
+    pop() {
+        if (!this.head) return undefined
+
+        const removedNode = this.tail
+        let current = this.head
+        let prev
+        while (current.next) {
+            prev = current
+            current = current.next
+        }
+        this.tail = prev
+        prev.next = null
+
+        this.length--
+        return removedNode
+    }
+
+    get(index) {
+        if (index === 0) return this.head
+        if (index > this.length) return null
+
+        let currentNode = this.head
+        for (let i = 0; i < index; i++) {
+            currentNode = currentNode.next
+        }
+
+        return currentNode
+    }
+
+    insert(index, val) {
+        const newNode = new Node(val)
+
+        if(index < 0) return false
+        if(index > this.length - 1) return false
+        if (index === 0) {
+            const oldHead = this.head
+            this.head = newNode
+            this.head.next = oldHead
+            return true
+        } else if(index === this.length - 1) {
+            this.push(val)
+            return true
+        }
+
+        const prevNode = this.get(index - 1)
+        const indexNode = this.get(index)
+
+        prevNode.next = newNode
+        newNode.next = indexNode
+
+        this.length++
+        return true
+    }
+}
+
+const singlyLinkedList = new SinglyLinkedList()
+
+// push
+// singlyLinkedList.push(5); // singlyLinkedList
+// singlyLinkedList.push(10)
+// singlyLinkedList.push(20)
+// console.log(singlyLinkedList)
+
+// pop
+// singlyLinkedList.pop()
+// console.log(singlyLinkedList)
+
+//get
+// singlyLinkedList.push(5).push(10).push(15).push(20);
+// console.log(singlyLinkedList.get(3))
+// singlyLinkedList.get(0).val // 5
+// singlyLinkedList.get(1).val // 10
+// singlyLinkedList.get(2).val // 15
+// singlyLinkedList.get(3).val // 20
+// singlyLinkedList.get(4) // null
+
+//insert
+// singlyLinkedList.push(5).push(10).push(15).push(20);
+// console.log(singlyLinkedList.insert(1,12)) // true
+// console.log(singlyLinkedList.insert(100,12)) // false

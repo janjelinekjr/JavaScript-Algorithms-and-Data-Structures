33e72e529b86a36e18178f311f2d73f6d3a050c5
Jan Jelínek <janjelinek.jr@gmail.com>
2023-04-13 15:59:01 +0200

    added set and removed methods to sll

--- Data Structures/12 Exercises/SinglyLinkedList.js
+++ Data Structures/12 Exercises/SinglyLinkedList.js
@@ -44,7 +44,7 @@ class SinglyLinkedList {
 
     get(index) {
         if (index === 0) return this.head
-        if (index > this.length) return null
+        if (index > this.length - 1) return null
 
         let currentNode = this.head
         for (let i = 0; i < index; i++) {
@@ -57,14 +57,14 @@ class SinglyLinkedList {
     insert(index, val) {
         const newNode = new Node(val)
 
-        if(index < 0) return false
-        if(index > this.length - 1) return false
+        if (index < 0) return false
+        if (index > this.length - 1) return false
         if (index === 0) {
             const oldHead = this.head
             this.head = newNode
             this.head.next = oldHead
             return true
-        } else if(index === this.length - 1) {
+        } else if (index === this.length - 1) {
             this.push(val)
             return true
         }
@@ -77,6 +77,32 @@ class SinglyLinkedList {
 
         this.length++
         return true
+    }
+
+    set(index, val) {
+        const currNode = this.get(index)
+
+        if (currNode) {
+            currNode.val = val
+            return true
+        }
+
+        return false
+    }
+
+    remove(index) {
+        if (index < 0 || index > this.length - 1) return undefined
+        if (index === 0) return this.shift()
+        if (index === this.length - 1) return this.pop()
+        const foundIndex = this.get(index)
+
+        if (foundIndex) {
+            const prevNode = this.get(index - 1)
+            prevNode.next = foundIndex.next
+        }
+
+        this.length--
+        return foundIndex
     }
 }
 
@@ -105,3 +131,13 @@ const singlyLinkedList = new SinglyLinkedList()
 // singlyLinkedList.push(5).push(10).push(15).push(20);
 // console.log(singlyLinkedList.insert(1,12)) // true
 // console.log(singlyLinkedList.insert(100,12)) // false
+
+// set
+// singlyLinkedList.push(5).push(10).push(15).push(20);
+// console.log(singlyLinkedList.set(1, 12)) // true
+// console.log(singlyLinkedList)
+
+// remove
+singlyLinkedList.push(5).push(10).push(15).push(20);
+console.log(singlyLinkedList.remove(1))
+console.log(singlyLinkedList)
